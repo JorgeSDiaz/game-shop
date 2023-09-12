3929f48dfe307ddf7c46c507eb27c7934a7acf06
Jorge Sz <saenzdiazj0@gmail.com>
2023-09-11 20:05:11 -0500

    add CRUD (Post Put Delete)

--- src/games/games.controller.ts
+++ src/games/games.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { GamesService } from './games.service';
 
 @Controller('games')
@@ -15,5 +15,20 @@ export class GamesController {
   @Get(':id')
   getGameById(@Param('id') id: string) {
     return this.gamesService.findGameById(id);
+  }
+
+  @Post()
+  addGame(@Body() game) {
+    return this.gamesService.add(game);
+  }
+
+  @Put(':id')
+  updateGame(@Param('id') id: string, @Body() game) {
+    return this.gamesService.update(id, game);
+  }
+
+  @Delete(':id')
+  deleteGame(@Param('id') id: string) {
+    return this.gamesService.remove(id);
   }
 }
